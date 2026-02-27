585dec4349a897444982cef616e451b6a68f6098
varunpandianp <varunpandian29@gmail.com>
2026-02-27 11:33:49 +0530

    simple intreset python code

---
+++ simpleintrest.py
@@ -0,0 +1,10 @@
+#simple interset Formula = SI=(principal*rate*time)/100
+
+p = float(input("enter the principal amount:"))
+r = float(input("enter the rate:"))
+t = float(input("Enter Time (in years): "))
+
+SI = p*r*t/100
+
+print("Simple intrest amount", SI)
+
