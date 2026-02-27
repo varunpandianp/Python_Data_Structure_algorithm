708f77a2607091f63da496ea72505f8318b66230
varunpandianp <varunpandian29@gmail.com>
2026-02-27 12:14:18 +0530

    find the largest of given number

---
+++ Introduction to python/largestof3numbers.py
@@ -0,0 +1,12 @@
+a = 20
+b = 10
+c = 30
+
+#find the larges of the given number
+
+if a>b and a>c :
+    print("Largest no is ",a)
+elif b>c :
+    print("Largest No is",b)
+else:
+    print("largest no is",c)
