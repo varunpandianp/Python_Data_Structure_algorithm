714abd88092d17303e82dece60c55c169ea42eb8
varunpandianp <varunpandian29@gmail.com>
2026-07-20 12:41:15 +0530

    program to cinvert fahrenheit ot celsis

---
+++ 01-data structure and algorithm/week-01/03-condition and loops/fahrenheittocelsis.py
@@ -0,0 +1,12 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+
+S =int(input())
+E =int(input())
+
+W =int(input())
+
+while S<=E:
+    C=int((5/9)*(S-32))
+    print(S,C)
+    S=S+W
