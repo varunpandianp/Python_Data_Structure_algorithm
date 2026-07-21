66dd30a86a8ed79966cf819cb8497695560ba948
varunpandianp <varunpandian29@gmail.com>
2026-07-21 22:14:29 +0530

    triangular patter with 3 variable nested loop to print triangular value by global and resign overwright the inner loop variable respective to the global value

---
+++ 01-data structure and algorithm/Week-02/triangularpattern02.py
@@ -0,0 +1,13 @@
+n = int(input("enter the value"))
+
+i = 1
+while i <=n:
+    j = 1
+    p = i
+    while j <= i:
+        print(p,end="")
+        j = j+1
+        p = p+1
+
+    print()
+    i=i+1
