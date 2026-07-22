658ab8086a8f79ce86557fd0fad71c77b4f030fe
varunpandianp <varunpandian29@gmail.com>
2026-07-22 12:30:21 +0530

    traingular pattern to print number using w=nested while loop

---
+++ 01-data structure and algorithm/Week-02/traingularpatter_print_numbers.py
@@ -0,0 +1,19 @@
+# Problem statement
+# Print the following pattern for the given N number of rows.
+#
+# Pattern for N = 4
+# 1
+# 22
+# 333
+# 4444
+
+n = int(input("Enter the number: "))
+i = 1
+while i <=n:
+    j = 1
+    p = i
+    while j <= i:
+        print(p,end="")
+        j = j+1
+    print()
+    i = i+1
