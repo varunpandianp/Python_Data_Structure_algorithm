bbed8cee64f30b19f6a344180bedd493598901cf
varunpandianp <varunpandian29@gmail.com>
2026-07-29 12:01:23 +0530

    number patter problem 02 using nested while loop and if condition

---
+++ 01-data structure and algorithm/Week-02/assignments.py
@@ -0,0 +1,47 @@
+# Problem statement
+# Print the following pattern for the given N number of rows.
+#
+# Pattern for N = 4
+# 1
+# 11
+# 111
+# 1111
+
+# n = int(input("Enter the value"))
+# i = 1
+
+# while i <=n:
+#     j = 1
+#     value=1
+#     while j <= i:
+#         print (value,end="")
+#         j = j+1
+#     print()
+#     i = i+1
+
+# Problem statement
+# Print the following pattern for the given N number of rows.
+#
+# Pattern for N = 4
+# 1
+# 11
+# 202
+# 3003
+
+n = int(input("Enter the value"))
+print(1)
+i = 1
+
+while i < n:
+    j = 0
+#     value=1
+    while (j<i+1):
+        if j == 0 or j == i:
+            print ( i,end="")
+#         elif j == i -1:
+#             print(i-1,end="")
+        else:
+            print(0,end="")
+        j = j+1
+    print()
+    i = i+1
