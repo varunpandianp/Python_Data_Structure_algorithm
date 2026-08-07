97029d8e2214d58e31ed0b2728460d3fa29c2aa9
varunpandianp <varunpandian29@gmail.com>
2026-08-07 23:43:10 +0530

    print number pyramid problem solved

---
+++ 01-data structure and algorithm/Week-02/03-More-on-loops/Problems-to-solve/print-number-pyramid.py
@@ -0,0 +1,34 @@
+# Problem statement
+# Print the following pattern for a given n.
+#
+# For eg. N = 6
+# 123456
+#   23456
+#     3456
+#       456
+#         56
+#           6
+#         56
+#       456
+#     3456
+#   23456
+# 123456
+
+n=int(input())
+
+for i in range(1,n+1):
+    #print Space
+    for s in range(i-1):
+        print(" ",end="")
+    #print nummber
+    for j in range(i,n+1):
+        print(j,end="")
+    print()
+for i in range(n-1,0,-1):
+     #print Space
+        for s in range(i-1):
+            print(" ",end="")
+        #print number
+        for j in range(i,n+1):
+            print(j,end="")
+        print()
