42c6e2e41ba474cdf402447b362af53d21339ab7
varunpandianp <varunpandian29@gmail.com>
2026-08-07 19:52:12 +0530

    continue keyword problem solves and studied use cases

---
+++ 01-data structure and algorithm/Week-02/03-More-on-loops/continue-keyword.py
@@ -0,0 +1,15 @@
+n = int(input())
+# i=2
+# while i <=n:
+#     if i%7==0:
+#         i=i+1
+#         continue
+#     print(i)
+#     i=i+1
+
+for i in range(2,n+1,1):
+    print(i)
+    if i % 7 ==0:
+
+        continue
+    print(i)
